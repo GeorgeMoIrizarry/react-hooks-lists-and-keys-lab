32640165b31c35a85208a570dacdbdf4397f6dc4
George Manuell Irizarry <mojomojoke9@gmail.com>
2024-04-23 13:47:04 -0400

    completed labs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkArr = links.map((linksArr) => {
+    return <a key={linksArr} href= {`#${linksArr}`}>{linksArr}</a>
+  })
+  return <nav>{linkArr}</nav>;
 }
 
 export default NavBar;
